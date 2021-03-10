8c111139004225950ea05c774e9187f50af4e1a2
Alan Massaro <alan.massar@gmail.com>
2021-03-10 16:24:22 +0100

    set home

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.background}>
+    <div>
       <h1 className="home-title">
         <span>SpaceX</span> News
       </h1>
